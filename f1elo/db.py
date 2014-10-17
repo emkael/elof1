07aedcdea97c01c5f1a1f3bc8d9f60a8516b42bf
emkael <emkael@tlen.pl>
2014-10-17 17:09:35 +0200

    * SQLite support + config examples

--- f1elo/db.py
+++ f1elo/db.py
@@ -7,7 +7,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 config = json.load(open(path.join(path.dirname(__main__.__file__), 'config', 'db.json')))
-engine = create_engine("mysql://{0[user]}:{0[pass]}@{0[host]}/{0[db]}?charset=utf8".format(config))
+if config['engine'] == 'mysql':
+    engine = create_engine("mysql://{0[user]}:{0[pass]}@{0[host]}/{0[db]}?charset=utf8".format(config))
+else:
+    engine = create_engine("sqlite:///{0[file]}".format(config))
 Session = sessionmaker(bind=engine)
 
 def find_driver(name, country, session):
