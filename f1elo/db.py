b06c5a9f139e55e0f725d80b1cfdd7a76aa873f3
emkael <emkael@tlen.pl>
2014-10-29 14:14:20 +0100

    * foreign key support for SQLite

--- f1elo/db.py
+++ f1elo/db.py
@@ -2,13 +2,16 @@ import json
 from os import path
 
 import __main__
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 config = json.load(open(path.join(path.dirname(__main__.__file__), 'config', 'db.json')))
 if config['engine'] == 'mysql':
     engine = create_engine("mysql://{0[user]}:{0[pass]}@{0[host]}/{0[db]}?charset=utf8".format(config))
-else:
+elif config['engine'] == 'sqlite':
     engine = create_engine("sqlite:///{0[file]}".format(config))
+    def fk_pragma(conn, record):
+        conn.execute('PRAGMA FOREIGN_KEYS=ON');
+    event.listen(engine, 'connect', fk_pragma)
 
 Session = sessionmaker(bind=engine)
