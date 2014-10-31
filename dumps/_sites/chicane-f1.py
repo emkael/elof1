9eabd017b4e7306bf59a9ef5776ed1d5a297e469
emkael <emkael@tlen.pl>
2014-10-31 21:51:55 +0100

    * ChicaneF1 title extraction

--- dumps/_sites/chicane-f1.py
+++ dumps/_sites/chicane-f1.py
@@ -7,7 +7,7 @@ from lxml import html
 def fetch(url):
     contents = urllib2.urlopen(url).read()
     tree = html.fromstring(contents)
-    title = tree.xpath("//title")[0].text
+    title = tree.xpath("//h1")[0].text + ' - ' + tree.xpath('//span[@class="subtitle"]')[0].text
     tables = tree.xpath("//table[@cellpadding=6]")
     print url
     print title
